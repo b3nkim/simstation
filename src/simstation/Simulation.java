80fc76fb3380c42286f3b14c55ee15554fc34df0
Bryant <bbautistabryant@gmail.com>
2023-04-12 18:20:25 -0700

    Added populate() to start() method and implemented getNeighbor method

--- src/simstation/Simulation.java
+++ src/simstation/Simulation.java
@@ -11,6 +11,7 @@ public class Simulation extends Model {
 	private List<Agent> agents = new ArrayList<Agent>();
 
 	public void start() {
+		populate();
 		for (Agent agent : agents) {
 			agent.start();
 		}
@@ -35,7 +36,25 @@ public class Simulation extends Model {
 	}
 
 	public Agent getNeighbor(Agent a, double radius) {
-		// TODO:
+		int startIndex = agents.indexOf(a);
+		int currentIndex = startIndex;
+		int count = agents.size();
+		Random random = new Random();
+
+		while (count > 0) {
+			int index = (currentIndex + random.nextInt(agents.size())) % agents.size();
+			if (index == startIndex) {
+				return null;
+			}
+			Agent neighbor = agents.get(index);
+
+			if (neighbor != a && a.distance(neighbor) <= radius) {
+				return neighbor;
+			}
+			currentIndex = index;
+			count--;
+		}
+		return null;
 	}
 
 	public void populate() {
@@ -56,7 +75,7 @@ public class Simulation extends Model {
 	private class ClockUpdater extends TimerTask {
 		public void run() {
 			clock++;
-			//changed();
+			changed();
 		}
 	}
 }
